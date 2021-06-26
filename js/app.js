72ed9d47311937302066e77ea488fb467d2afb83
samaranand <samarss2015@gmail.com>
2021-06-26 23:42:09 +0530

    update-4

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ const url = 'https://samaranand-blogs.herokuapp.com/';
 const values = document.getElementById('values')
 
 
+const _createdAt = (v) =>{
+    const d = new Date(v);
+    return `${['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][d.getDay()]} ${String(d.getDate()).padStart(2, '0')}/${String(d.getMonth()).padStart(2, '0')}/${d.getFullYear()}`;
+}
+
 const getElUI = (e) =>{
     return `<div class="card mt-4">
                 <div class="card-body"> 
@@ -9,12 +14,12 @@ const getElUI = (e) =>{
                         ${e.title}
                     </h4>
                     <div class="card-subtitle text-muted mb-2">
-                        ${e.createdAt}
+                        ${ _createdAt(e.createdAt)}
                     </div>
                     <div class="card-text mb-2">
                         ${e.description}
                     </div>
-                    <a href="${url+'blogs/'+e.slug}" class="btn btn-primary"  target="_blank" >Read More</a>
+                    <a href="${url+'blogs/'+e.slug}" class="btn btn-primary">Read More</a>
                 </div>
             </div>`
 }
